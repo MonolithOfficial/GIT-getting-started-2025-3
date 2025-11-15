41102b5105961ad29ebab1156a62e6b5a7b32bd1
cotneaburjania@gmail.com <cotneaburjania@gmail.com>
2025-11-15 11:29:16 +0400

    Created maven project and added Main class

---
+++ src/main/java/ge/tbc/testautomation/Main.java
@@ -0,0 +1,8 @@
+package ge.tbc.testautomation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("I am JAVA");
+    }
+}
